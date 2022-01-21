bb1dacb8ef0ff87f015cc1607224e8a7fe5f4628
KyleA <arbourkyle@gmail.com>
2022-01-20 23:29:25 -0500

    git status told me to do stuff

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 //PSEUDO TODO:
-//Make a prompt on click event for the first user input
+//Make a prompt on click event for  user input
 
 //Prompt 1: Length of password 8 - 16 characters
 
@@ -21,107 +21,68 @@
 
 //Display into card-body with password ID
 
-/* let charLength = prompt("Choose password length between 8 and 16");
-let charLower = prompt("Include lowercase?");
-let charUpper = prompt("Include uppercase?");
-let charNum = prompt("Include numbers?");
-let charSpec = prompt("Include special characters?"); */
 
 
 
 
 
-// Assignment Code
 var password = document.querySelector("#password");
 var generateBtn = document.querySelector("#generate");
 
-// Write password to the #password input
+
+// Generate password 
+
+
 function writePassword() {
+  let charLength = prompt("Password length between 8-16 characters?")
+  let charLower = confirm("Include lowercase?")
+  let charUpper = confirm("Include uppercase?");
+  let charNum = confirm("Include numbers?");
+  let charSpec = confirm("Include symbols?");
+  let passVar = ""
+  let password = ""
+  
+  
+  
+  // Validity check on variables
+  if(isNaN (parseInt(charLength))) {
+    alert("Please input a number between 8 and 16");
+    return;
+    }
+  if(parseInt(charLength) < 8 || parseInt(charLength) > 16) {
+    alert("Your password is either too short or long. Please keep your password between 8-16 characters in length.");
+    return;
+    }
+    
+  if(charLower === true) {
+    var lowerCaseLetters = "abcdefghijklmnopqrstuvwxyz"
+    passVar += lowerCaseLetters
+    }
+  if(charUpper === true) {
+     var upperCaseLetters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+     passVar += upperCaseLetters
+     }
+  if(charNum === true) {
+    var numbersTrue = "1234567890"
+    passVar += numbersTrue
+    }
+  if(charSpec === true) {
+    var symbolsTrue = '"~!@#$%^&*()_+{}|-="'
+    passVar += symbolsTrue
+    }
 
-var char = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()"
 
-/* let chars = {
 
-charUpper: "ABCDEFGHIJKLMNOPQRSTUVWXYZ",
-charLower: "abcdefghijklmnopqrstuvwxyz",
-charNum: "0123456789",
-charSpec: "!@#$%^&*()"
 
-};
-
-function charValue(char) {
-
-  Object.values(chars);
-  return chars = char;
-
+// Loop through password variables.
+for (var i = 0; i < charLength; i++) {
+  var randomNumber = Math.floor(Math.random() * passVar.length);
+  password += passVar.substring(randomNumber, randomNumber +1);
+ }
+ 
+       document.getElementById("password").value = password;
+       
 }
-
-let char = charValue(); */
-
-
-
-
-// var passwordLength = "";
-var password = "";
-// var char = "";
-
-/* if(prompt("Include uppercase?") == "yes" || "y") {
-  char = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-}
-
-else if(prompt("Include lowercase?") == "yes" || "y") {
-  char = "abcdefghijklmnopqrstuvwxyz";
-}
-
-else if(prompt("Include numbers?") == "yes" || "y") {
-  char = "0123456789";
-}
-else if(prompt("Include numbers?") == "yes" || "y") {
-  char = "!@#$%^&*()";
-}
-else
-{
-  return;
-} */
-
-let passwordLength = prompt("Please enter password length between 8-16 characters");
-
-
-//function in a function messy and no working
-
-/* function upper() {
-  prompt("include uppercase?");
-  char += prompt.value;
-}
-
-upper(); */
-
-
-//set var initial to +1 because the result was always one more than the user input, starting at +1 reduces the index by 1, thereby subtracting one
-
-for (var i = +1; i <= passwordLength; i++) {
-    var randomNumber = Math.floor(Math.random() * char.length);
-    password += char.substring(randomNumber, randomNumber +1);
-   }
-/* 
-   for (var i = 0; i <= passwordLength; i++) {
-    var randomNumber = Math.floor(Math.random() * charLower.length);
-    password += charLower.substring(randomNumber, randomNumber +1);
-   }
-   for (var i = 0; i <= passwordLength; i++) {
-    var randomNumber = Math.floor(Math.random() * charNum.length);
-    password += charNum.substring(randomNumber, randomNumber +1);
-   }
-   for (var i = 0; i <= passwordLength; i++) {
-    var randomNumber = Math.floor(Math.random() * charSpec.length);
-    password += charSpec.substring(randomNumber, randomNumber +1);
-   } */
-      
-   
-   document.getElementById("password").value = password;
-      
-}
-
 
 
 // Add event listener to generate button
