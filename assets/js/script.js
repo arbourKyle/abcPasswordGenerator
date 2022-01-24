c515cfcf478e7d3fa61d3ea01c408c02091704d1
KyleA <arbourkyle@gmail.com>
2022-01-24 16:06:55 -0500

    updated password length requirement and organized folders

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,31 +1,3 @@
-//PSEUDO TODO:
-//Make a prompt on click event for  user input
-
-//Prompt 1: Length of password 8 - 16 characters
-
-//Prompt 2: Ask if to include lowerCase?
-
-//Prompt 3: Ask if to include upperCase?
-
-//Prompt 4: Ask if to include numbers?
-
-//Prompt 5: Ask if to include special characters?
-
-//TODO:
-//When user gives input, evaluate the input for:
-  //boolean: is the input valid? error message or continue to next step?
-  //selection: a random generation of requested values
-  //value change: update an empty object with each generated values
-
-//Concat or stringify: 5 key-value pairs can be summed and displayed as password
-
-//Display into card-body with password ID
-
-
-
-
-
-
 var password = document.querySelector("#password");
 var generateBtn = document.querySelector("#generate");
 
@@ -34,7 +6,7 @@ var generateBtn = document.querySelector("#generate");
 
 
 function writePassword() {
-  let charLength = prompt("Password length between 8-16 characters?")
+  let charLength = prompt("Password length between 8-128 characters?")
   let charLower = confirm("Include lowercase?")
   let charUpper = confirm("Include uppercase?");
   let charNum = confirm("Include numbers?");
@@ -46,10 +18,10 @@ function writePassword() {
   
   // Validity check on variables
   if(isNaN (parseInt(charLength))) {
-    alert("Please input a number between 8 and 16");
+    alert("Please input a number between 8 and 128");
     return;
     }
-  if(parseInt(charLength) < 8 || parseInt(charLength) > 16) {
+  if(parseInt(charLength) < 8 || parseInt(charLength) > 128) {
     alert("Your password is either too short or long. Please keep your password between 8-16 characters in length.");
     return;
     }
